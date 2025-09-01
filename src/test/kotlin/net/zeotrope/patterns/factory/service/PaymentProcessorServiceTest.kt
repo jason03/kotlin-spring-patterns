ed3339d071a13fe9b860ffb4529948bda5f5beaf
Jason Harris <jasonhh@mac.com>
2025-09-01 14:54:28 +0100

    added tests for factory

--- src/test/kotlin/net/zeotrope/patterns/factory/service/PaymentProcessorServiceTest.kt
+++ src/test/kotlin/net/zeotrope/patterns/factory/service/PaymentProcessorServiceTest.kt
@@ -1,5 +1,6 @@
 package net.zeotrope.patterns.factory.service
 
+import net.zeotrope.patterns.factory.BankTransferProcessor
 import net.zeotrope.patterns.factory.CreditCardProcessor
 import net.zeotrope.patterns.factory.OnlineServiceProcessor
 import net.zeotrope.patterns.factory.PaymentResponse
@@ -8,13 +9,19 @@ import net.zeotrope.patterns.factory.PaymentType
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.api.assertThrows
+import org.mockito.Mock
+import org.mockito.Mockito.any
+import org.mockito.Mockito.anyString
 import org.mockito.Mockito.verify
 import org.mockito.Mockito.`when`
+import org.mockito.Spy
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.SpyBean
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.bean.override.mockito.MockitoBean
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean
 import java.math.BigDecimal
 import kotlin.test.assertEquals
 
@@ -28,6 +35,12 @@ class PaymentProcessorServiceTest(
 
     @MockitoBean
     private lateinit var creditCardProcessor: CreditCardProcessor
+
+    @MockitoSpyBean
+    private lateinit var serviceSpy: PaymentProcessorService
+
+    @Mock
+    private lateinit var onlineServiceProcessor: OnlineServiceProcessor
 
     @Test
     fun `should verify that process payment is delegated to the correct processor`() {
@@ -44,5 +57,33 @@ class PaymentProcessorServiceTest(
             { assertEquals("Payment was successful.", response.message) }
         )
         verify(creditCardProcessor).process(BigDecimal.valueOf(100.00))
+    }
+
+    @Test
+    fun `should verify that process payment is delegated to the correct processor by string type`() {
+        val paymentValue = BigDecimal.valueOf(10.75)
+
+        `when`(
+            onlineServiceProcessor.process(paymentValue)
+        ).thenReturn(PaymentResponse(PaymentStatus.SUCCESS, "Mock payment was successful."))
+
+        `when`(
+            serviceSpy.processPayment(paymentValue, "onlineService")
+        ).then { call -> onlineServiceProcessor.process(call.arguments[0] as BigDecimal) }
+
+        val response = serviceSpy.processPayment(paymentValue, "onlineService")
+
+        assertAll(
+            { assertEquals(PaymentStatus.SUCCESS, response.status) },
+            { assertEquals("Mock payment was successful.", response.message) }
+        )
+        verify(onlineServiceProcessor).process(paymentValue)
+    }
+
+    @Test
+    fun `should throw exception when payment type is not supported`() {
+        assertThrows<IllegalArgumentException> {
+            service.processPayment(BigDecimal.valueOf(100.00), "invalid")
+        }
     }
 }
